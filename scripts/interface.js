308c563c35c3ac2a1587725712b76cf565fb7143
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-16 17:52:02 +0800

    Create interface.js

---
+++ scripts/interface.js
@@ -0,0 +1,25 @@
+import {
+    MonthOfLeapYear,
+    MonthOfComYear,
+    DayChange,
+    Week,
+    ShowList
+} from './config.js'
+
+import {
+    isLeapYear,
+    getDayNum,
+    getDayOfOne,
+    getLastMonth,
+    getNextMonth,
+    getLastYear,
+    getNextYear,
+    getLastTenYear,
+    getNextTenYear,
+    addClass
+} from "./utils.js";
+
+
+export {
+
+}
