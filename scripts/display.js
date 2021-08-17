fbf9c434ea03a1944572970085926004234b81cf
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-17 17:54:16 +0800

    Update display.js

--- scripts/display.js
+++ scripts/display.js
@@ -1,5 +1,5 @@
 import {
-    Week,
+    WEEK,
 } from './config.js'
 
 import {
@@ -13,15 +13,15 @@ import {
 
 // 显示当前时间的函数
 function displayTime() {
-    const NowDate = new Date();
-    let hour = NowDate.getHours();
-    let minute = NowDate.getMinutes();
+    const nowDate = new Date();
+    let hour = nowDate.getHours();
+    let minute = nowDate.getMinutes();
     minute = minute < 10 ? `0${minute}` : minute;
-    let second = NowDate.getSeconds();
+    let second = nowDate.getSeconds();
     second = second < 10 ? `0${second}` : second;
-    let ClockStr = `${hour}:${minute}:${second}`;
+    let clockStr = `${hour}:${minute}:${second}`;
     let clock = document.getElementById("clock");
-    clock.innerHTML = ClockStr;
+    clock.innerHTML = clockStr;
 }
 
 function showClock() {
@@ -29,29 +29,28 @@ function showClock() {
 }
 
 // 显示今天日期的函数
-function showToday(TodayDate, node) {
-    let today = `${TodayDate.year}年${TodayDate.month + 1}月${TodayDate.date}日`;
+function showToday(todayDate, node) {
+    let today = `${todayDate.year}年${todayDate.month + 1}月${todayDate.date}日`;
     node.innerHTML = today;
 }
 
 // 显示x历头
-function showHead(ShowDate, node) {
+function showHead(showDate, node) {
     let str = ""
-    if (ShowDate.showing === 0) {
-        str = `${ShowDate.year}年${ShowDate.month + 1}月`
-    } else if (ShowDate.showing === 1) {
-        str = `${ShowDate.year}年`
-    } else if (ShowDate.showing === 2) {
-        str = `${ShowDate.year - ShowDate.year % 10}-${(ShowDate.year - ShowDate.year % 10) + 9}`
+    if (showDate.showing === 0) {
+        str = `${showDate.year}年${showDate.month + 1}月`
+    } else if (showDate.showing === 1) {
+        str = `${showDate.year}年`
+    } else if (showDate.showing === 2) {
+        str = `${showDate.year - showDate.year % 10}-${(showDate.year - showDate.year % 10) + 9}`
     }
     node.innerHTML = str
-    console.log(str)
 }
 
 // 生成一个月的日历骨架
-function createSingleMonth(ShowDate) {
+function createSingleMonth(showDate) {
     // 获取上个月对象
-    let LastMonth = getLastMonth(ShowDate.year, ShowDate.month)
+    let LastMonth = getLastMonth(showDate.year, showDate.month)
     let table = document.createElement("table")
     let Str = ""
     // 生成表内容
@@ -61,9 +60,9 @@ function createSingleMonth(ShowDate) {
         if (i % 7 === 0) {
             Str += "<tr>"
         }
-        if (i < ShowDate.dayOfOne) {
-            Str += `<td>${LastMonth.dayNum - ShowDate.dayOfOne + i + 1}</td>`
-        } else if (i >= ShowDate.dayOfOne && i < ShowDate.dayOfOne + ShowDate.dayNum) {
+        if (i < showDate.dayOfOne) {
+            Str += `<td>${LastMonth.dayNum - showDate.dayOfOne + i + 1}</td>`
+        } else if (i >= showDate.dayOfOne && i < showDate.dayOfOne + showDate.dayNum) {
             Str += `<td>${j}</td>`
             j++
         } else {
@@ -80,21 +79,21 @@ function createSingleMonth(ShowDate) {
 
 
 // 显示全部日历骨架
-function createCalendar(ShowDate, content) {
+function createCalendar(showDate, content) {
     // 生成表头
     let week = document.createElement("div")
     week.setAttribute("id", "week")
     for (let k = 0; k < 7; k++) {
         let item = document.createElement("div")
         item.setAttribute("class", "item")
-        item.innerHTML = Week[k]
+        item.innerHTML = WEEK[k]
         week.appendChild(item)
     }
 
     // 生成三个表
-    const table_last = createSingleMonth(getLastMonth(ShowDate.year, ShowDate.month))
-    const table_now = createSingleMonth(ShowDate)
-    const table_next = createSingleMonth(getNextMonth(ShowDate.year, ShowDate.month))
+    const table_last = createSingleMonth(getLastMonth(showDate.year, showDate.month))
+    const table_now = createSingleMonth(showDate)
+    const table_next = createSingleMonth(getNextMonth(showDate.year, showDate.month))
 
     // 轮播图容器
     const carousel_container = document.createElement("div")
@@ -116,28 +115,28 @@ function createCalendar(ShowDate, content) {
 
 
 // 更改日历显示效果
-function changeCalendarCss(TodayDate, ShowDate, content) {
+function changeCalendarCss(todayDate, showDate, content) {
     const table = content.getElementsByTagName("table")[1]
-    let CalendarList = table.getElementsByTagName("td")
+    let calendarList = table.getElementsByTagName("td")
     for (let i = 0; i < 42; i++) {
-        if (i < ShowDate.dayOfOne || i >= (ShowDate.dayOfOne + ShowDate.dayNum)) {
-            addClass(CalendarList[i], "NotNow")
+        if (i < showDate.dayOfOne || i >= (showDate.dayOfOne + showDate.dayNum)) {
+            addClass(calendarList[i], "NotNow")
         }
     }
-    if (ShowDate.year === TodayDate.year && ShowDate.month === TodayDate.month && ShowDate.date === TodayDate.date) {
-        addClass(CalendarList[ShowDate.dayOfOne + TodayDate.date - 1], "CurDateItem")
-        let str = `<div class="CurDate">${TodayDate.date}</div>`
-        CalendarList[ShowDate.dayOfOne + TodayDate.date - 1].innerHTML = str
+    if (showDate.year === todayDate.year && showDate.month === todayDate.month && showDate.date === todayDate.date) {
+        addClass(calendarList[showDate.dayOfOne + todayDate.date - 1], "CurDateItem")
+        let str = `<div class="CurDate">${todayDate.date}</div>`
+        calendarList[showDate.dayOfOne + todayDate.date - 1].innerHTML = str
     }
 }
 
 
 // 显示日历
-function showCalendar(ShowDate, TodayDate, content, content_head) {
+function showCalendar(showDate, todayDate, content, content_head) {
     // TODO 显示上中下三个月的日历
-    createCalendar(ShowDate, content)
-    changeCalendarCss(TodayDate, ShowDate, content)
-    showHead(ShowDate, content_head)
+    createCalendar(showDate, content)
+    changeCalendarCss(todayDate, showDate, content)
+    showHead(showDate, content_head)
 }
 
 // 显示月历骨架
@@ -165,38 +164,38 @@ function createMonth(content) {
 }
 
 // 更改月历显示效果,绑定事件处理函数
-function changeMonthCss(ShowDate, TodayDate, content, content_head) {
+function changeMonthCss(showDate, todayDate, content, content_head) {
     const tableList = content.getElementsByTagName("table")
     for (let i = 0; i < 3; i++) {
         addClass(tableList[i], "month_content")
     }
-    const MonthList = tableList[1].getElementsByTagName("td")
+    const monthList = tableList[1].getElementsByTagName("td")
     for (let i = 0; i < 16; i++) {
-        MonthList[i].addEventListener("click", handleMonthClick.bind(MonthList[i], ShowDate, TodayDate, content, content_head))  // 通过bind实现函数传参
+        monthList[i].addEventListener("click", handleMonthClick.bind(monthList[i], showDate, todayDate, content, content_head))  // 通过bind实现函数传参
         if (i >= 12) {
-            addClass(MonthList[i], "NotNow")
+            addClass(monthList[i], "NotNow")
         }
     }
-    if (ShowDate.year === TodayDate.year && ShowDate.month === TodayDate.month) {
-        addClass(MonthList[ShowDate.month], "CurMonthItem")
+    if (showDate.year === todayDate.year && showDate.month === todayDate.month) {
+        addClass(monthList[showDate.month], "CurMonthItem")
     }
 }
 
 // 显示月历的函数
-function showMonth(ShowDate, TodayDate, content, content_head) {
+function showMonth(showDate, todayDate, content, content_head) {
     createMonth(content)
-    changeMonthCss(ShowDate, TodayDate, content, content_head)
-    showHead(ShowDate, content_head)
+    changeMonthCss(showDate, todayDate, content, content_head)
+    showHead(showDate, content_head)
 }
 
 // 显示年历骨架
-function createYear(ShowDate, content) {
+function createYear(showDate, content) {
     // 生成表格骨架
     let index = [3, 0, 1, 2]
-    let leftYear = ShowDate.year - ShowDate.year % 10
+    let leftYear = showDate.year - showDate.year % 10
     let leftYearIndex = index[leftYear % 4]
     let str = ""
-    const FirstYear = leftYear - leftYearIndex - 32
+    const firstYear = leftYear - leftYearIndex - 32
     for (let i = 0; i < 48; i++) {
         if (i % 16 === 0) {
             str += "<table>"
@@ -204,7 +203,7 @@ function createYear(ShowDate, content) {
         if (i % 4 === 0) {
             str += "<tr>"
         }
-        str += `<td>${FirstYear + i}</td>`
+        str += `<td>${firstYear + i}</td>`
         if (i % 4 === 3) {
             str += "</tr>"
         }
@@ -225,58 +224,58 @@ function createYear(ShowDate, content) {
 }
 
 // 更改年历显示效果，绑定事件处理函数
-function changeYearCss(leftYearIndex, ShowDate, TodayDate, content, content_head) {
+function changeYearCss(leftYearIndex, showDate, todayDate, content, content_head) {
     const tableList = content.getElementsByTagName("table")
     for (let i = 0; i < 3; i++) {
         addClass(tableList[i], "month_content")
     }
     const YearList = tableList[1].getElementsByTagName("td")
     for (let i = 0; i < 16; i++) {
-        YearList[i].addEventListener("click", handleYearClick.bind(YearList[i], ShowDate, TodayDate, content, content_head))
+        YearList[i].addEventListener("click", handleYearClick.bind(YearList[i], showDate, todayDate, content, content_head))
         if (i < leftYearIndex || i > leftYearIndex + 9) {
             addClass(YearList[i], "NotNow")
         }
     }
-    if (ShowDate.year === TodayDate.year) {
-        addClass(YearList[leftYearIndex + TodayDate.year % 10], "CurMonthItem")
+    if (showDate.year === todayDate.year) {
+        addClass(YearList[leftYearIndex + todayDate.year % 10], "CurMonthItem")
     }
 }
 
 // 显示年历
-function showYear(ShowDate, TodayDate, content, content_head) {
-    const leftYearIndex = createYear(ShowDate, content)
-    changeYearCss(leftYearIndex, ShowDate, TodayDate, content, content_head)
-    showHead(ShowDate, content_head)
+function showYear(showDate, todayDate, content, content_head) {
+    const leftYearIndex = createYear(showDate, content)
+    changeYearCss(leftYearIndex, showDate, todayDate, content, content_head)
+    showHead(showDate, content_head)
 }
 
 // 处理点击月份
-function handleMonthClick(ShowDate, TodayDate, content, content_head) {
-    ShowDate.showing = 0
+function handleMonthClick(showDate, todayDate, content, content_head) {
+    showDate.showing = 0
     const month = this.childNodes[0].nodeValue.slice(0, -1)
-    ShowDate.month = month - 1
-    ShowDate.dayNum = getDayNum(ShowDate.year, ShowDate.month)
-    ShowDate.dayOfOne = getDayOfOne(ShowDate.year, ShowDate.month);
+    showDate.month = month - 1
+    showDate.dayNum = getDayNum(showDate.year, showDate.month)
+    showDate.dayOfOne = getDayOfOne(showDate.year, showDate.month);
 
-    showCalendar(ShowDate, TodayDate, content, content_head)  //更新日历
+    showCalendar(showDate, todayDate, content, content_head)  //更新日历
 }
 
 // 处理点击年份
-function handleYearClick(ShowDate, TodayDate, content, content_head) {
-    ShowDate.showing = 1
+function handleYearClick(showDate, todayDate, content, content_head) {
+    showDate.showing = 1
     const year = this.childNodes[0].nodeValue
-    ShowDate.year = year
-    ShowDate.dayNum = getDayNum(ShowDate.year, ShowDate.month)
-    ShowDate.dayOfOne = getDayOfOne(ShowDate.year, ShowDate.month);
+    showDate.year = year
+    showDate.dayNum = getDayNum(showDate.year, showDate.month)
+    showDate.dayOfOne = getDayOfOne(showDate.year, showDate.month);
 
-    showMonth(ShowDate, TodayDate, content, content_head)  //显示月历
+    showMonth(showDate, todayDate, content, content_head)  //显示月历
 }
 
 
 // 显示当月日历
-function showNowMonth(ShowDate, TodayDate, content, content_head) {
-    ShowDate = Object.assign(ShowDate, TodayDate)
-    ShowDate.showing = 0
-    showCalendar(ShowDate, TodayDate, content, content_head)
+function showNowMonth(showDate, todayDate, content, content_head) {
+    showDate = Object.assign(showDate, todayDate)
+    showDate.showing = 0
+    showCalendar(showDate, todayDate, content, content_head)
 }
 
 
@@ -297,5 +296,4 @@ export {
     changeYearCss,
     showYear,
     showClock,
-
 }
