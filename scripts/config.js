fe585a6033c236143b27135ab925ddd3b47b9f03
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-17 17:54:15 +0800

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,13 +1,13 @@
-const MonthOfLeapYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const MonthOfComYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const DayChange = [6, 0, 1, 2, 3, 4, 5];
-const Week = ["一", "二", "三", "四", "五", "六", "日"]
-const ShowList = ["day", "month", "year"]
+const MONTHOFLEAPYEAR = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const MONTHOFCOMYEAR = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const DAYCHENGE = [6, 0, 1, 2, 3, 4, 5];
+const WEEK = ["一", "二", "三", "四", "五", "六", "日"]
+const SHOWLIST = ["day", "month", "year"]
 
 export {
-    MonthOfLeapYear,
-    MonthOfComYear,
-    DayChange,
-    Week,
-    ShowList
+    MONTHOFLEAPYEAR,
+    MONTHOFCOMYEAR,
+    DAYCHENGE,
+    WEEK,
+    SHOWLIST
 }
