fca8076303880fac552b0d71ae5fd6b1ce135d71
coldbird <73277480+swusj@users.noreply.github.com>
2021-08-19 19:11:34 +0800

    Update statemachine.js

--- scripts/statemachine.js
+++ scripts/statemachine.js
@@ -12,16 +12,16 @@ var CalendarstateMachine = {
     // 当前状态
     currentState: SHOWING_STATE.DAY,
     toDayCalendar: function (showDate, todayDate, content, content_head) {
-        this.currentState = SHOWING_STATE.DAY,
-            showCalendar(showDate, todayDate, content, content_head)
+        this.currentState = SHOWING_STATE.DAY
+        showCalendar(showDate, todayDate, content, content_head)
     },
     toMonthCalendar: function (showDate, todayDate, content, content_head) {
-        this.currentState = SHOWING_STATE.MONTH,
-            showMonth(showDate, todayDate, content, content_head)
+        this.currentState = SHOWING_STATE.MONTH
+        showMonth(showDate, todayDate, content, content_head)
     },
     toYearCalender: function (showDate, todayDate, content, content_head) {
-        this.currentState = SHOWING_STATE.YEAR,
-            showYear(showDate, todayDate, content, content_head)
+        this.currentState = SHOWING_STATE.YEAR
+        showYear(showDate, todayDate, content, content_head)
     }
 }
 
